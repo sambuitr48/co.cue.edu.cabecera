279018134389feb62b30ea3aa978b8cd10fcc62f
sambuitr48 <buitragoosoriosamuel@gmail.com>
2024-04-09 23:23:52 -0500

    second commit 9/04

--- src/main/java/controllers/Test.java
+++ src/main/java/controllers/Test.java
@@ -15,16 +15,16 @@ public class Test extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        String metodoHttp = req.getMethod();
-        String requestUri = req.getRequestURI();
-        String requestUrl = req.getRequestURL().toString();
-        String contexPath = req.getContextPath();
-        String servletPath = req.getServletPath();
-        String ipCliente = req.getRemoteAddr();
-        String ip = req.getLocalAddr();
-        int port = req.getLocalPort();
-        String scheme = req.getScheme();
-        String host = req.getHeader("host");
+        String metodoHttp = req.getMethod(); //Is used to determine whether the request is GET or a POST
+        String requestUri = req.getRequestURI(); //Is used to determine the URI of the request
+        String requestUrl = req.getRequestURL().toString(); //Is used to get the URL of the request
+        String contexPath = req.getContextPath(); //Used when you need to build links or relative paths within your web application
+        String servletPath = req.getServletPath(); //Used when you need to perform specific actions on your servlet based  on the path used in the request
+        String ipCliente = req.getRemoteAddr(); //Used to obtain the IP address of the client making http request
+        String ip = req.getLocalAddr(); //Used to get the IP address of the server on which the web application is running
+        int port = req.getLocalPort(); //used to get the port number on the server on the server on which the web application is running
+        String scheme = req.getScheme(); //Used to get the schema of the incoming http request
+        String host = req.getHeader("host"); //Used to get the "Host" header of an http request
         String url = scheme + "://" + host + contexPath + servletPath;
         String url2 = scheme + "://" + ip + ":" + port + contexPath + servletPath;
         try (PrintWriter out = resp.getWriter()) {
